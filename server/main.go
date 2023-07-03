dbfef229806cfa16bd040ea26a697c79ee6da2b6
claustra01 <shinya430.g@gmail.com>
2023-07-03 12:49:38 +0900

    :sparkles: backend sample api

---
+++ server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/claustra01/hackz_megamouse/server/handler"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("[ERROR] ", err)
+	}
+
+	e := echo.New()
+
+	e.GET("/api/sample/", handler.Sample)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
