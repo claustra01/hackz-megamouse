90cdbf949486e663fd4f584a9d28c98c8466a5c7
claustra01 <shinya430.g@gmail.com>
2023-07-06 02:53:21 +0900

    :sparkles: sample onclick action

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/kyoto-framework/kyoto/v2"
@@ -16,12 +17,26 @@ type CUUIDState struct {
 }
 
 func CUUID(ctx *kyoto.Context) (state CUUIDState) {
-	// Fetch uuid data
-	resp, _ := http.Get("http://httpbin.org/uuid")
-	data := map[string]string{}
-	json.NewDecoder(resp.Body).Decode(&data)
-	// Set state
-	state.UUID = data["uuid"]
+	// Define uuid loader
+	uuid := func() string {
+		resp, _ := http.Get("http://httpbin.org/uuid")
+		data := map[string]string{}
+		json.NewDecoder(resp.Body).Decode(&data)
+		return data["uuid"]
+	}
+	// Handle action
+	handled := kyoto.Action(ctx, "Reload", func(args ...any) {
+		// We will just set a new uuid and will print a log
+		// It's not makes a lot of sense now, but it's just a demonstration example
+		state.UUID = uuid()
+		log.Println("New uuid was issued:", state.UUID)
+	})
+	// Prevent further execution if action handled
+	if handled {
+		return
+	}
+	// Default loading behavior
+	state.UUID = uuid()
 	// Return
 	return
 }
@@ -42,8 +57,7 @@ func PIndex(ctx *kyoto.Context) (state PIndexState) {
 }
 
 func main() {
-	// Register page
 	kyoto.HandlePage("/", PIndex)
-	// Serve
+	kyoto.HandleAction(CUUID)
 	kyoto.Serve(":3000")
 }
