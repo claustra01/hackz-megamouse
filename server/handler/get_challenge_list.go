6d5347c29be825aaa7d81ec56820ca170104695e
claustra01 <shinya430.g@gmail.com>
2023-07-15 23:55:53 +0900

    :zap: sort challenges by category and value

--- server/handler/get_challenge_list.go
+++ server/handler/get_challenge_list.go
@@ -9,33 +9,32 @@ import (
 )
 
 type OmmitedChallenge struct {
-	Id        uint      `json:"id"`
-	Title  string    `json:"title"`
-	Category   string    `json:"category"`
-	Description     string    `json:"description"`
-	FilePath     string      `json:"filepath"`
-	ConnectionInfo   string      `json:"connection_info"`
-	Value   int      `json:"value"`
-	IsVisible   bool      `json:"is_visible"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Id             uint      `json:"id"`
+	Title          string    `json:"title"`
+	Category       string    `json:"category"`
+	Description    string    `json:"description"`
+	FilePath       string    `json:"filepath"`
+	ConnectionInfo string    `json:"connection_info"`
+	Value          int       `json:"value"`
+	IsVisible      bool      `json:"is_visible"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
-
 
 func ommit_c(challenges []db.Challenge) []OmmitedChallenge {
 	var list []OmmitedChallenge
 	for _, v := range challenges {
 		u := OmmitedChallenge{
-			Id:        v.Id,
-			Title:  v.Title,
-			Category:   v.Category,
-			Description:     v.Description,
-			FilePath:     v.FilePath,
-			ConnectionInfo:   v.ConnectionInfo,
-			Value: v.Value,
-			IsVisible: v.IsVisible,
-			CreatedAt: v.CreatedAt,
-			UpdatedAt: v.UpdatedAt,
+			Id:             v.Id,
+			Title:          v.Title,
+			Category:       v.Category,
+			Description:    v.Description,
+			FilePath:       v.FilePath,
+			ConnectionInfo: v.ConnectionInfo,
+			Value:          v.Value,
+			IsVisible:      v.IsVisible,
+			CreatedAt:      v.CreatedAt,
+			UpdatedAt:      v.UpdatedAt,
 		}
 		list = append(list, u)
 	}
@@ -43,13 +42,28 @@ func ommit_c(challenges []db.Challenge) []OmmitedChallenge {
 }
 
 func GetChallengeList(c echo.Context) error {
+
+	var categories []db.Category
 	var challenges []db.Challenge
-		if err := db.DB.Find(&challenges).Error; err != nil {
-			return c.JSON(http.StatusInternalServerError, echo.Map{
-				"message": "Failed to fetch challenges",
-			})
-		}else{
-			list := ommit_c(challenges)
-			return c.JSON(http.StatusOK, list)
+	if err := db.DB.Find(&categories).Error; err != nil {
+		// return 500
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "Database Error: " + err.Error(),
+		})
+
+	} else {
+		// get challenges in each category, return 200
+		var chal []db.Challenge
+		for _, v := range categories {
+			if err := db.DB.Where("category = ?", v.Name).Order("value ASC").Find(&chal).Error; err != nil {
+				// return 500
+				return c.JSON(http.StatusInternalServerError, echo.Map{
+					"message": "Database Error: " + err.Error(),
+				})
+			}
+			challenges = append(challenges, chal...)
 		}
+		list := ommit_c(challenges)
+		return c.JSON(http.StatusOK, list)
+	}
 }
