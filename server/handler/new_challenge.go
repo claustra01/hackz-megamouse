c960de99ba9f634bf8f600fe209b275ae0059f87
claustra01 <shinya430.g@gmail.com>
2023-07-15 23:34:28 +0900

    :sparkles: create categories table

--- server/handler/new_challenge.go
+++ server/handler/new_challenge.go
@@ -11,66 +11,94 @@ import (
 )
 
 func NewChallenge(c echo.Context) error {
-	type Challenge struct{
-		Title string `json:"title"`
-		Category string `json:"category"`
-		Description string `json:"description"`
-		FilePath string `json:"filepath"`
+	type Challenge struct {
+		Title          string `json:"title"`
+		Category       string `json:"category"`
+		Description    string `json:"description"`
+		FilePath       string `json:"filepath"`
 		ConnectionInfo string `json:"connectioninfo"`
-		Flag string `json:"flag"`
-		Value int `json:"value"`
+		Flag           string `json:"flag"`
+		Value          int    `json:"value"`
+		IsVisible      bool   `json:"is_visible"`
 	}
 
 	user := c.Get("user").(*jwt.Token)
 	claims := user.Claims.(jwt.MapClaims)
 	userid := claims["id"].(float64)
+
 	var userdata db.User
 	if err := db.DB.Where("id = ?", userid).First(&userdata).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
+			// return 404
 			return c.JSON(http.StatusNotFound, echo.Map{
 				"message": "User Not Found",
 			})
 		} else {
+			// return 500
 			return c.JSON(http.StatusInternalServerError, echo.Map{
 				"message": "Database Error: " + err.Error(),
 			})
 		}
-	}else{
-		if userdata.IsAdmin{
+
+	} else {
+		if userdata.IsAdmin {
 			obj := new(Challenge)
 			if err := c.Bind(obj); err != nil {
+				// return 400
 				return c.JSON(http.StatusBadRequest, echo.Map{
 					"message": "Json Format Error: " + err.Error(),
 				})
 			}
-		
-			if util.HasEmptyField(obj, "Title", "Category","Flag","Value") {
+
+			if util.HasEmptyField(obj, "Title", "Category") {
 				// return 400
 				return c.JSON(http.StatusBadRequest, echo.Map{
 					"message": "Missing Required Field",
 				})
 			}
-			
-			new := db.Challenge{
-				Title: obj.Title,
-				Category: obj.Category,
-				Description: obj.Description,
-				FilePath: obj.FilePath,
-				ConnectionInfo: obj.ConnectionInfo,
-				Flag: obj.Flag,
-				Value: obj.Value,
+
+			var category db.Category
+			if err := db.DB.Where("name = ?", obj.Category).First(&category).Error; err != nil {
+				if err == gorm.ErrRecordNotFound {
+					// create new category
+					db.DB.Create(&db.Category{
+						Name: obj.Category,
+					})
+				} else {
+					// return 500
+					return c.JSON(http.StatusInternalServerError, echo.Map{
+						"message": "Database Error: " + err.Error(),
+					})
+				}
 			}
-		
+
+			// create challenge, return 201
+			new := db.Challenge{
+				Title:          obj.Title,
+				Category:       obj.Category,
+				Description:    obj.Description,
+				FilePath:       obj.FilePath,
+				ConnectionInfo: obj.ConnectionInfo,
+				Flag:           obj.Flag,
+				Value:          obj.Value,
+				IsVisible:      obj.IsVisible,
+			}
 			db.DB.Create(&new)
-			return c.JSON(http.StatusOK, echo.Map{
-				"title": obj.Title,
-				"category": obj.Category,
-				"description": obj.Description,
-				"filepath": obj.FilePath,
-				"connection_info": obj.ConnectionInfo,
-				"value": obj.Value,
+			return c.JSON(http.StatusCreated, echo.Map{
+				"id":              new.Id,
+				"title":           new.Title,
+				"category":        new.Category,
+				"description":     new.Description,
+				"filepath":        new.FilePath,
+				"connection_info": new.ConnectionInfo,
+				"value":           new.Value,
+				"is_visible":      new.IsVisible,
+				"created_at":      new.CreatedAt,
+				"updated_at":      new.UpdatedAt,
 			})
-		}else{
+
+		} else {
+			// return 403
 			return c.JSON(http.StatusNotFound, echo.Map{
 				"message": "admin only",
 			})
