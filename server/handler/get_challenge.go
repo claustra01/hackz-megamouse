df8b615fadf2b92ab771fa2ec707e04fb24374d7
nnnnn81 <nnnnnagi81@gmail.com>
2023-07-15 12:18:13 +0900

    🩹 fix error message & omit

--- server/handler/get_challenge.go
+++ server/handler/get_challenge.go
@@ -5,15 +5,39 @@ import (
 
 	"github.com/claustra01/hackz-megamouse/server/db"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 // e.GET("/challenges/:id", getChallenge)(main.go)
 func GetChallenge(c echo.Context) error {
 
+
 	var challenge db.Challenge
 	id := c.Param("id")
 	if err := db.DB.Where("id = ?", id).First(&challenge).Error; err != nil {
-		return c.String(http.StatusNotFound, "Challenge not found")
+		if err == gorm.ErrRecordNotFound {
+			return c.JSON(http.StatusNotFound, echo.Map{
+				"message": "Challenge not found",
+			})
+
+		} else {
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"message": "Database Error: " + err.Error(),
+			})
+		}
 	}else{
-		return c.JSON(http.StatusOK, challenge)
+		o_challenge := OmmitedChallenge{
+			Id: challenge.Id,
+			Title: challenge.Title,
+			Category: challenge.Category,
+			Description: challenge.Description,
+			FilePath: challenge.FilePath,
+			ConnectionInfo: challenge.ConnectionInfo,
+			Value: challenge.Value,
+			IsVisible: challenge.IsVisible,
+			CreatedAt: challenge.CreatedAt,
+			UpdatedAt: challenge.UpdatedAt,
+
+		}
+		return c.JSON(http.StatusOK, o_challenge)
 	}
 }
