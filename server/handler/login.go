a172cf4480f6dd16d5ab2780783c2f6fd4c6e1b4
claustra01 <shinya430.g@gmail.com>
2023-07-15 17:33:30 +0900

    ⚡ auth return content

--- server/handler/login.go
+++ server/handler/login.go
@@ -12,14 +12,16 @@ import (
 )
 
 func Login(c echo.Context) error {
+
 	type Body struct {
-		Username string `json:"username"`
-		Email string `json:"email"`
+		Email    string `json:"email"`
 		Password string `json:"password"`
 	}
+
 	// parse
 	obj := new(Body)
 	if err := c.Bind(obj); err != nil {
+		// return 400
 		return c.JSON(http.StatusBadRequest, echo.Map{
 			"message": "Json Format Error: " + err.Error(),
 		})
@@ -31,42 +33,45 @@ func Login(c echo.Context) error {
 			"message": "Missing Required Field",
 		})
 	}
-	// emailが存在するか
+
+	// ユーザーが存在するか
 	var user db.User
 	if err := db.DB.Where("email = ?", obj.Email).First(&user).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
-			return c.JSON(http.StatusBadRequest, echo.Map{
+			// return 404
+			return c.JSON(http.StatusNotFound, echo.Map{
 				"message": "User Not Found",
 			})
 		} else {
+			// return 500
 			return c.JSON(http.StatusInternalServerError, echo.Map{
 				"message": "Database Error: " + err.Error(),
 			})
 		}
+
 	} else {
-		// パスワードあってるか 
-		if (user.Username == obj.Username && user.Password == obj.Password) {
+		if user.Email == obj.Email && user.Password == obj.Password {
 			// ペイロード作成
 			claims := jwt.MapClaims{
-				"id": user.Id,
-				"exp":     time.Now().Add(time.Hour * 24).Unix(),
+				"id":  user.Id,
+				"exp": time.Now().Add(time.Hour * 24).Unix(),
 			}
-		
 			// トークン生成
 			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-		
 			// トークンに署名を付与
 			tokenString, err := token.SignedString([]byte("SECRET_KEY"))
 			if err != nil {
 				return err
 			}
-		
+			// return 200
 			return c.JSON(http.StatusOK, echo.Map{
 				"token": tokenString,
 			})
+
 		} else {
-			return c.JSON(http.StatusBadRequest,echo.Map{
-				"message": "username or password is incorrect",
+			// return 401
+			return c.JSON(http.StatusUnauthorized, echo.Map{
+				"message": "Unauthorized",
 			})
 		}
 	}
