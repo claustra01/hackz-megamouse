fc65f288766315d5c866bd96daa9bd0f0eb041f6
claustra01 <shinya430.g@gmail.com>
2023-07-15 10:49:54 +0900

    :sparkles: create tables

--- server/db/migrate.go
+++ server/db/migrate.go
@@ -3,7 +3,16 @@ package db
 import "log"
 
 func Migrate() {
+
 	DB.Exec("DROP TABLE IF EXISTS users")
+	DB.Exec("DROP TABLE IF EXISTS challenges")
+	DB.Exec("DROP TABLE IF EXISTS submissions")
+	DB.Exec("DROP TABLE IF EXISTS solves")
+
 	DB.AutoMigrate(&User{})
+	DB.AutoMigrate(&Challenge{})
+	DB.AutoMigrate(&Submission{})
+	DB.AutoMigrate(&Solves{})
+
 	log.Print("[INFO] DB Migrated!")
 }
