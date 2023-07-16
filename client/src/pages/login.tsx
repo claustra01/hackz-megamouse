13d3ae28c9a91e980b88fbbe658afd52762512ca
nnnnn81 <nnnnnagi81@gmail.com>
2023-07-16 21:42:21 +0900

    🩹 move components

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -2,7 +2,7 @@ import React, { useState, ChangeEvent, FormEvent } from 'react';
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 import { useRouter } from 'next/router';
-import Header from './components/Header';
+import Header from '../components/Header';
 
 interface ApiResponse {
   token?: string;
