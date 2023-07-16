7f5d5292996297cbfa642e1627331cbbc29b215e
claustra01 <shinya430.g@gmail.com>
2023-07-16 21:14:50 +0900

    :sparkles: token auth

--- client/src/pages/AuthContext.tsx
+++ client/src/pages/AuthContext.tsx
@@ -1,4 +1,7 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
+// AuthContext.tsx
+
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import { useCookies } from 'react-cookie';
 
 type AuthStatus = {
   isLoggedIn: boolean;
@@ -15,11 +18,52 @@ const initialAuthStatus: AuthStatus = {
 const AuthContext = createContext<AuthStatus>(initialAuthStatus);
 
 type AuthProviderProps = {
-  children: ReactNode; // childrenの型を追加
+  children: ReactNode;
 };
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [authStatus, setAuthStatus] = useState<AuthStatus>(initialAuthStatus);
+  const [cookies] = useCookies(['token']); // トークンのCookieを取得
+
+  useEffect(() => {
+    // ここでCookieからトークンを取得してヘッダーに付与し、/authへのGETリクエストを行う
+    const fetchAuthToken = async () => {
+      try {
+        if (cookies.token) {
+          const response = await fetch('/api/auth', {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${cookies.token}`, // トークンをヘッダーに付与
+            },
+          });
+
+          if (response.status === 200) {
+            const data = await response.json();
+
+            setAuthStatus({
+              isLoggedIn: true,
+              isAdmin: data.is_admin,
+              userId: data.id,
+            });
+          } else {
+            setAuthStatus((prevStatus) => ({
+              ...prevStatus,
+              isLoggedIn: false,
+            }));
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching auth token:', error);
+      }
+    };
+
+    fetchAuthToken();
+  }, [cookies.token]);
+
+  // authStatusの変更を監視してコンソールに出力
+  useEffect(() => {
+    console.log('AuthStatus:', authStatus);
+  }, [authStatus]);
 
   return (
     <AuthContext.Provider value={authStatus}>
