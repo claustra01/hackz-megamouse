36b3793de99286533394a4bd13b2ced007800973
nnnnn81 <nnnnnagi81@gmail.com>
2023-07-16 21:02:09 +0900

    🩹 rename components

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Header from './components/header';
+import Header from './components/Header';
 
 const Login = () => {
   const [email, setEmail] = useState('');
