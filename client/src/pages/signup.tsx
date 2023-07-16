97b529ea35ea636b4650625b9ee271c907299c52
claustra01 <shinya430.g@gmail.com>
2023-07-17 04:55:39 +0900

    :lipstick: header, top, login, signup

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -1,79 +1,7 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import styled from 'styled-components';
-
-const SignupContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  background-color: #f8f8f8;
-`;
-
-const SignupTitle = styled.h2`
-  font-size: 32px;
-  font-weight: bold;
-  color: #333;
-  margin-bottom: 20px;
-`;
-
-const SignupForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 300px;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
-const FormField = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 15px;
-
-  & label {
-    font-weight: bold;
-    margin-bottom: 5px;
-  }
-
-  & input {
-    padding: 8px;
-    font-size: 14px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-`;
-
-const ErrorMessage = styled.p`
-  color: #ff0000;
-  font-size: 14px;
-  margin-top: 10px;
-`;
-
-const SignupButton = styled.button`
-  padding: 10px 20px;
-  font-size: 16px;
-  font-weight: bold;
-  color: #fff;
-  background-color: #66b2ff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  font-family: 'Tektur', sans-serif;
-
-  &:hover {
-    background-color: #4682b4;
-  }
-
-  &:focus {
-    outline: none;
-  }
-`;
+import { Title, Form, FormField, Button, ErrorMessage, Container } from '@/styles/styledComponents';
 
 const Signup: React.FC = () => {
   const [username, setUsername] = useState<string>('');
@@ -123,9 +51,9 @@ const Signup: React.FC = () => {
   };
 
   return (
-    <SignupContainer>
-      <SignupTitle>Signup</SignupTitle>
-      <SignupForm onSubmit={handleSubmit}>
+    <Container>
+      <Title>Signup</Title>
+      <Form onSubmit={handleSubmit}>
         <FormField>
           <label>Username:</label>
           <input type="text" value={username} onChange={handleUsernameChange} />
@@ -138,10 +66,10 @@ const Signup: React.FC = () => {
           <label>Password:</label>
           <input type="password" value={password} onChange={handlePasswordChange} />
         </FormField>
-        <SignupButton type="submit">Signup</SignupButton>
+        <Button type="submit">Signup</Button>
         {responseMessage && <ErrorMessage>{responseMessage}</ErrorMessage>}
-      </SignupForm>
-    </SignupContainer>
+      </Form>
+    </Container>
   );
 };
 
---
+++ client/src/styles/styledComponents.ts
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f8f8f8;
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 30px;
+`;
+
+export const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  background-color: #333;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-family: 'Tektur', sans-serif;
+
+  &:hover {
+    background-color: #ffac00;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > :not(:last-child) {
+    margin-bottom: 10px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const FormField = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+
+  & label {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  & input {
+    padding: 8px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+`;
+
+export const ErrorMessage = styled.p`
+  color: #ff0000;
+  font-size: 14px;
+  margin-top: 10px;
+`;
