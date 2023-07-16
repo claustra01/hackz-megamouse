f5b65c1e46ff6b16210e961ae90d5104ea0d5a0a
nnnnn81 <nnnnnagi81@gmail.com>
2023-07-16 20:42:39 +0900

    ✨add imitation page

---
+++ client/src/pages/challenges.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Header from './components/header';
+
+const Challenges = () => {
+  return (
+    <div>
+      <Header />
+      <h2>challenges</h2>
+    </div>
+  );
+};
+
+export default Challenges;
---
+++ client/src/pages/scoreboard.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Header from './components/header';
+
+const Scoreboard = () => {
+  return (
+    <div>
+      <Header />
+      <h2>scoreboard</h2>
+    </div>
+  );
+};
+
+export default Scoreboard;
