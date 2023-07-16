bd0f0729af1d733ea8fa9e4a81eec4f5094f0477
nnnnn81 <nnnnnagi81@gmail.com>
2023-07-16 22:16:08 +0900

    🩹 fix sample data

--- client/src/pages/challenges.tsx
+++ client/src/pages/challenges.tsx
@@ -3,9 +3,9 @@ import Header from '../components/Header';
 import ChallengeList from '../components/ChallengeList';
 
 const response = [
-  { title: 'Card 1', description: 'Description 1', value: 500 },
-  { title: 'Card 2', description: 'Description 2', value: 400 },
-  { title: 'Card 3', description: 'Description 3', value: 100 },
+  { title: 'Card 1', category: 'web', description: 'Description 1', value: 500 },
+  { title: 'Card 2', category: 'web', description: 'Description 2', value: 400 },
+  { title: 'Card 3', category: 'web', description: 'Description 3', value: 100 },
 ];
 
 const Challenges = () => {
