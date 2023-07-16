de143e2e143d9404667e960bf058cab0adb6c6a1
claustra01 <shinya430.g@gmail.com>
2023-07-16 23:25:47 +0900

    :lipstick: signup page design

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -1,28 +1,96 @@
+import React from 'react';
 import Link from 'next/link';
 import Header from '../components/Header';
+import styled from 'styled-components';
+
+const SignupContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f8f8f8;
+`;
+
+const SignupTitle = styled.h2`
+  font-size: 32px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const SignupForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const FormField = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+
+  & label {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  & input {
+    padding: 8px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+`;
+
+const SignupButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  background-color: #66b2ff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #4682b4;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 const Signup = () => {
   return (
-    <div>
+    <>
       <Header />
-
-      <h2>Signup</h2>
-      <form>
-        <div>
-          <label>Username:</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" />
-        </div>
-        <button type="submit">signup</button>
-      </form>
-    </div>
+      <SignupContainer>
+        <SignupTitle>Signup</SignupTitle>
+        <SignupForm>
+          <FormField>
+            <label>Username:</label>
+            <input type="text" />
+          </FormField>
+          <FormField>
+            <label>Email:</label>
+            <input type="email" />
+          </FormField>
+          <FormField>
+            <label>Password:</label>
+            <input type="password" />
+          </FormField>
+          <SignupButton type="submit">Signup</SignupButton>
+        </SignupForm>
+      </SignupContainer>
+    </>
   );
 };
 
