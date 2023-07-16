a95d268666dbf0b09e245feedb1c125c9486dd6f
nnnnn81 <nnnnnagi81@gmail.com>
2023-07-16 23:43:39 +0900

    ✨ add modal components

--- client/src/components/Challenge.tsx
+++ client/src/components/Challenge.tsx
@@ -1,19 +1,38 @@
 import { Challenge } from '../lib/contents';
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './Modal';
+import Panel from './Panel';
 
 interface ChallengeProps {
   data: Challenge
 }
 
 const ChallengeCard = ({ data }: ChallengeProps) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className="card">
-      <div className="card-content">
-        <h2 className="card-title">{data.title}</h2>
-        <p className="card-description">{data.description}</p>
-        <p className="card-value">{data.value}</p>
-      </div>
-    </div>
+    <>
+      <button onClick={handleOpenModal}>
+        <div className="card">
+          <div className="card-content">
+            <h2 className="card-title">{data.title}</h2>
+            <p className="card-description">{data.description}</p>
+            <p className="card-value">{data.value}</p>
+          </div>
+        </div>
+      </button>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <Panel />
+      </Modal>
+    </>
   );
 };
 
---
+++ client/src/components/Panel.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Panel = () => {
+  return (
+    <div className="panel">
+      <h2 className="panel-title">title</h2>
+      <div className="panel-content">content</div>
+      <form action="">
+        <p>flag</p>
+        <input type="text" />
+        <button>submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Panel;
