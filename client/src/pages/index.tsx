a279fc393993f4a7aa44b30659058cf2c319d664
claustra01 <shinya430.g@gmail.com>
2023-07-17 12:45:34 +0900

    :lipstick: main title animation

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,27 @@
 import { Button, ButtonContainer, Container, Title } from '@/styles/styledComponents';
 import Link from 'next/link';
+import styled, { keyframes } from 'styled-components';
+
+const fadeInAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const AnimatedTitle = styled(Title)`
+  font-size: 72px;
+  animation: ${fadeInAnimation} 1s ease-in-out;
+`;
 
 const MainPage = () => {
   return (
     <Container>
-      <Title>Megamouth CTF</Title>
+      <AnimatedTitle>Megamouth CTF</AnimatedTitle>
       <ButtonContainer>
         <Link href="/login">
           <Button>Login</Button>
