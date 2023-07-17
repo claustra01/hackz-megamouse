ca85d599b6123bff319005d9429a43615799ac76
claustra01 <shinya430.g@gmail.com>
2023-07-17 09:40:40 +0900

    :sparkles: [web] forbidden

---
+++ challenge/web/1/index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+const FLAG = process.env.FLAG;
+
+app.get("/", (req, res, next) => {
+    return res.send('FLAG はこちら: <a href="/flag">/flag</a>');
+});
+
+const block = (req, res, next) => {
+    if (req.path.includes('/flag')) {
+        return res.send(403, 'Forbidden :(');
+    }
+
+    next();
+}
+
+app.get("/flag", block, (req, res, next) => {
+    return res.send(FLAG);
+})
+
+var server = app.listen(50000);
