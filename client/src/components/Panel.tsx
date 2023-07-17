6268ddf03fef679310f0a865692e070a983a09bc
claustra01 <shinya430.g@gmail.com>
2023-07-17 11:08:06 +0900

    :bug: uninputable textbox

--- client/src/components/Panel.tsx
+++ client/src/components/Panel.tsx
@@ -4,6 +4,79 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 
+const PanelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  background-color: #fff; /* 雰囲気に合った色 */
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 300px;
+
+  .panel-title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 15px;
+  }
+
+  .panel-content {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+
+  .panel-filepath {
+    font-size: 14px;
+    color: #666;
+    margin-bottom: 5px;
+  }
+
+  .panel-connectioninfo {
+    font-size: 14px;
+    color: #666;
+    margin-bottom: 15px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    p {
+      font-size: 14px;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+
+    input {
+      padding: 8px;
+      font-size: 14px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      margin-bottom: 10px;
+    }
+
+    button {
+      padding: 10px 20px;
+      font-size: 16px;
+      font-weight: bold;
+      color: #fff;
+      background-color: #333; /* 雰囲気に合った色 */
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+
+      &:hover {
+        background-color: #ffac00;
+      }
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+`;
 
 const Panel = (data: any) => {
   const [cookies] = useCookies(['token']);
@@ -39,100 +112,23 @@ const Panel = (data: any) => {
       console.error('Error:', error);
     }
   };
-  const PanelContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-    background-color: #fff; /* 雰囲気に合った色 */
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    width: 300px;
-  
-    .panel-title {
-      font-size: 24px;
-      font-weight: bold;
-      margin-bottom: 15px;
-    }
-  
-    .panel-content {
-      font-size: 16px;
-      margin-bottom: 10px;
-    }
-  
-    .panel-filepath {
-      font-size: 14px;
-      color: #666;
-      margin-bottom: 5px;
-    }
-  
-    .panel-connectioninfo {
-      font-size: 14px;
-      color: #666;
-      margin-bottom: 15px;
-    }
-  
-    form {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-  
-      p {
-        font-size: 14px;
-        font-weight: bold;
-        margin-bottom: 5px;
-      }
-  
-      input {
-        padding: 8px;
-        font-size: 14px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        margin-bottom: 10px;
-      }
-  
-      button {
-        padding: 10px 20px;
-        font-size: 16px;
-        font-weight: bold;
-        color: #fff;
-        background-color: #333; /* 雰囲気に合った色 */
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-  
-        &:hover {
-          background-color: #ffac00;
-        }
-  
-        &:focus {
-          outline: none;
-        }
-      }
-    }
-  `;
+
   return (
     <PanelContainer>
       <h2 className="panel-title">{data.data.title}</h2>
       <div className="panel-description">{data.data.description}</div>
       <div className="panel-filepath">{data.data.filepath}</div>
       <div className="panel-connectioninfo">{data.data.connection_info}</div>
-      {!iscollect &&
-        <div >
+      {!iscollect && (
+        <div>
           <p>flag</p>
           <input type="text" onChange={handleInputChange} />
           <button onClick={handleSubmit}>submit</button>
         </div>
-      }
-      {(iscollect == false && isSubmit) && <p>Failed...</p>}
-      {iscollect &&
-        <p>Success!</p>
-      }
-    </ PanelContainer>
-
-
-
+      )}
+      {iscollect === false && isSubmit && <p>Failed...</p>}
+      {iscollect && <p>Success!</p>}
+    </PanelContainer>
   );
 };
 
