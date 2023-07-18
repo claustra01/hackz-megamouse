48167dbdabd24ddac789dde5cbe0d245fae58fee
claustra01 <shinya430.g@gmail.com>
2023-07-18 11:25:44 +0900

    :sparkles: [web] pass traversal

--- challenge/web/1/index.js
+++ challenge/web/1/index.js
@@ -1,22 +1,29 @@
-var express = require("express");
-var app = express();
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const FLAG = process.env.FLAG;
-
-app.get("/", (req, res, next) => {
-    return res.send('FLAG: <a href="/flag">/flag</a>');
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, req.url);
+  
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('<h1>404 Not Found</h1>');
+    } else {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/html' });
+          res.end('<h1>500 Internal Server Error</h1>');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    }
+  });
 });
 
-const block = (req, res, next) => {
-    if (req.path.includes('/flag')) {
-      return res.send(403, 'Forbidden');
-    }
-
-    next();
-}
-
-app.get("/flag", block, (req, res, next) => {
-    return res.send(FLAG);
-})
-
-var server = app.listen(50000);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
